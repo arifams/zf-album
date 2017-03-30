27e220819d4d0dd191f2dfa7704e5f626892d36a
arifams <arif.amsterdam@gmail.com>
2017-03-30 11:52:14 +0200

    zf backbone; album

---
+++ Album/src/Module.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Album;
+
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+    // getConfig() and getServiceConfig methods are here
+
+    // Add this method:
+    public function getControllerConfig()
+    {
+        return [
+            'factories' => [
+                Controller\AlbumController::class => function($container) {
+                    return new Controller\AlbumController(
+                        $container->get(Model\AlbumTable::class)
+                    );
+                },
+            ],
+        ];
+    }
+}
